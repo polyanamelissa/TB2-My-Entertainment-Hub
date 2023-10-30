f6663f90ce6ae90db0a108fb598f5805f79ca894
Tigas <134016106+TiagoHast@users.noreply.github.com>
2023-10-30 17:21:29 -0300

    Update index-jquery.js

--- pagina-inicial-main/index-jquery.js
+++ pagina-inicial-main/index-jquery.js
@@ -1,1 +1,8 @@
 $(document).ready(function() {
+  const $itemForm = $("#item-form");
+  const $itemName = $("#item-name");
+  const $category = $("#category");
+  const $itemList = $("#item-list");
+  const $filterForm = $("#filter-form");
+  const $filterCategory = $("#filter-category");
+
