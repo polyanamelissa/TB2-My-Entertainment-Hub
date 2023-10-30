ba3919662d348ef647e49a76ce9bffc28e957da9
Tigas <134016106+TiagoHast@users.noreply.github.com>
2023-10-30 17:27:17 -0300

    Update index-jquery.js

--- pagina-inicial-main/index-jquery.js
+++ pagina-inicial-main/index-jquery.js
@@ -1,58 +1,58 @@
 $(document).ready(function() {
-  const $itemForm = $("#item-form");
-  const $itemName = $("#item-name");
-  const $category = $("#category");
-  const $itemList = $("#item-list");
-  const $filterForm = $("#filter-form");
-  const $filterCategory = $("#filter-category");
-
-  $itemForm.on("submit", function(event) {
-        event.preventDefault();
-        const name = $itemName.val();
-        const selectedCategory = $category.val();
-        const $itemDiv = $("<div class='item'></div>").text(`${name} - ${selectedCategory}`);
-        $itemList.append($itemDiv);
-        $itemName.val("");
-
-       const items = JSON.parse(localStorage.getItem("items")) || [];
-        items.push({ name, category: selectedCategory });
-        localStorage.setItem("items", JSON.stringify(items));
-        updateCategoryFilter();
-    });
-
-    function updateCategoryFilter() {
-        const items = JSON.parse(localStorage.getItem("items")) || [];
-        const categories = items.map(item => normalizeString(item.category.toLowerCase()));
-        const uniqueCategories = [...new Set(categories)];
-
-        $filterCategory.html("");
-        uniqueCategories.forEach(category => {
-            $filterCategory.append(`<option value="${category}">${category}</option>`);
-        });
-    }
-    $filterForm.on("submit", function(event) {
-        event.preventDefault();
-        const selectedCategory = $filterCategory.val();
-        const items = JSON.parse(localStorage.getItem("items")) || [];
-
-        if (selectedCategory === "all") {
-            
-            $itemList.html("");
-            items.forEach(item => {
-                $itemList.append($("<div class='item'></div>").text(`${item.name} - ${item.category}`));
-            });
-        } else {
-            
-            $itemList.html("");
-            items.forEach(item => {
-                if (normalizeString(item.category.toLowerCase()) === selectedCategory) {
-                    $itemList.append($("<div class='item'></div>").text(`${item.name} - ${item.category}`));
-                }
-            });
-        }
-    });
-  function normalizeString(str) {
-        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    }
-    updateCategoryFilter();
-});
+    const $itemForm = $("#item-form");
+    const $itemName = $("#item-name");
+    const $category = $("#category");
+    const $itemList = $("#item-list");
+    const $filterForm = $("#filter-form");
+    const $filterCategory = $("#filter-category");
+  
+    $itemForm.on("submit", function(event) {
+          event.preventDefault();
+          const name = $itemName.val();
+          const selectedCategory = $category.val();
+          const $itemDiv = $("<div class='item'></div>").text(`${name} - ${selectedCategory}`);
+          $itemList.append($itemDiv);
+          $itemName.val("");
+  
+         const items = JSON.parse(localStorage.getItem("items")) || [];
+          items.push({ name, category: selectedCategory });
+          localStorage.setItem("items", JSON.stringify(items));
+          updateCategoryFilter();
+      });
+  
+      function updateCategoryFilter() {
+          const items = JSON.parse(localStorage.getItem("items")) || [];
+          const categories = items.map(item => normalizeString(item.category.toLowerCase()));
+          const uniqueCategories = [...new Set(categories)];
+  
+          $filterCategory.html("");
+          uniqueCategories.forEach(category => {
+              $filterCategory.append(`<option value="${category}">${category}</option>`);
+          });
+      }
+      $filterForm.on("submit", function(event) {
+          event.preventDefault();
+          const selectedCategory = $filterCategory.val();
+          const items = JSON.parse(localStorage.getItem("items")) || [];
+  
+          if (selectedCategory === "all") {
+              
+              $itemList.html("");
+              items.forEach(item => {
+                  $itemList.append($("<div class='item'></div>").text(`${item.name} - ${item.category}`));
+              });
+          } else {
+              
+              $itemList.html("");
+              items.forEach(item => {
+                  if (normalizeString(item.category.toLowerCase()) === selectedCategory) {
+                      $itemList.append($("<div class='item'></div>").text(`${item.name} - ${item.category}`));
+                  }
+              });
+          }
+      });
+    function normalizeString(str) {
+          return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      }
+      updateCategoryFilter();
+  });
