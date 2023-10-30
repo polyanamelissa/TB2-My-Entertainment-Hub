9fe055209d6ad7426bd3bee9a405ce37d9d3aadc
Tigas <134016106+TiagoHast@users.noreply.github.com>
2023-10-30 17:22:12 -0300

    Update index-jquery.js

--- pagina-inicial-main/index-jquery.js
+++ pagina-inicial-main/index-jquery.js
@@ -6,3 +6,18 @@ $(document).ready(function() {
   const $filterForm = $("#filter-form");
   const $filterCategory = $("#filter-category");
 
+  $itemForm.on("submit", function(event) {
+        event.preventDefault();
+        const name = $itemName.val();
+        const selectedCategory = $category.val();
+        const $itemDiv = $("<div class='item'></div>").text(`${name} - ${selectedCategory}`);
+        $itemList.append($itemDiv);
+        $itemName.val("");
+
+       const items = JSON.parse(localStorage.getItem("items")) || [];
+        items.push({ name, category: selectedCategory });
+        localStorage.setItem("items", JSON.stringify(items));
+
+        // Atualizar a lista de categorias no filtro
+        updateCategoryFilter();
+    });
