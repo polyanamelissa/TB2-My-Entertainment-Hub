2ad931ac88d27a107f239c839e33348382a0312f
linaa23 <133915216+linaa23@users.noreply.github.com>
2023-11-14 17:04:12 -0300

    Update cadastro.js

--- cadastro-login-main/cadastro.js
+++ cadastro-login-main/cadastro.js
@@ -11,13 +11,13 @@ function exibirBalaoMensagem(mensagem, tipo) {
         
       }
 
-      // exibe o balão de mensagem
+    
       balaoMensagem.style.display = 'block';
 
-      // oculta o balão após alguns segundos
+      
       setTimeout(function() {
         balaoMensagem.style.display = 'none';
-      }, 7000); 
+      }, 5000); 
     }
 
     function salvarCadastro() {
@@ -26,6 +26,7 @@ function exibirBalaoMensagem(mensagem, tipo) {
       var senha = document.getElementById("password").value;
 
       // verifique se os campos não estão vazios
+
       if (nome && email && senha) {
         var cadastro = {
           username: nome,
@@ -44,39 +45,3 @@ function exibirBalaoMensagem(mensagem, tipo) {
       }
     }
 
-/*
-
-// código novo gpt
-
-function salvarCadastro() {
-  var email = document.querySelector('input[name="email"]').value;
-  var senha = document.querySelector('input[name="password"]').value;
-
-  if (email.trim() !== "" && senha.trim() !== "") {
-    // Recupera os dados do localStorage
-    var usuariosCadastrados =
-      JSON.parse(localStorage.getItem("usuarios")) || [];
-
-    // Verifica se o email já está cadastrado
-    var emailExistente = usuariosCadastrados.some(function (usuario) {
-      return usuario.email === email;
-    });
-
-    if (emailExistente) {
-      alert("Este email já está cadastrado. Por favor, use outro.");
-    } else {
-      // Adiciona o novo usuário à lista
-      usuariosCadastrados.push({ email: email, password: senha });
-
-      // Salva a lista atualizada no localStorage
-      localStorage.setItem("usuarios", JSON.stringify(usuariosCadastrados));
-
-      alert("Cadastro realizado com sucesso!");
-    }
-  } else {
-    alert("Por favor, preencha todos os campos.");
-  }
-}
-
-
- */
