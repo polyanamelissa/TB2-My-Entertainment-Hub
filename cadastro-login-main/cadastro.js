1a10cc8184cc0a3eb669d345fa170bfa2c08e408
Tigas <134016106+TiagoHast@users.noreply.github.com>
2023-11-22 18:32:27 -0300

    Update cadastro.js

--- cadastro-login-main/cadastro.js
+++ cadastro-login-main/cadastro.js
@@ -18,24 +18,41 @@ function exibirBalaoMensagem(mensagem, tipo) {
     }, 3000);
 }
 
-function salvarCadastro() {
+function salvarCadastro(a, b, c) {
+
     var nome = document.getElementById("username").value;
     var email = document.getElementById("email").value;
     var senha = document.getElementById("password").value;
 
-    // verifique se os campos não estão vazios
+    // veruificar se os campos não estão vazios
     if (nome && email && senha) {
-        exibirBalaoMensagem("Cadastro salvo com sucesso!", "sucesso");
         var cadastro = {
             username: nome,
             email: email,
             password: senha,
         };
+        $.ajax({
+            url: "cadlogin.php",
+            dataType: "html",
+            data: {nome: a, user: b, senha: c},
+            method: "post",
+            success: function (statusEmail) {
+                if (statusEmail) {
+                    exibirBalaoMensagem("Cadastro salvo com sucesso!", "sucesso");
+                    var cadastroString = JSON.stringify(cadastro);
+                    localStorage.setItem("cadastro", cadastroString);
+                }else
+                    exibirBalaoMensagem("E-mail já cadastrado anteriormente.", "erro");
+                    window.location("cadastro.html");
+            }
+        });
 
-        var cadastroString = JSON.stringify(cadastro);
 
-        localStorage.setItem("cadastro", cadastroString);
     } else {
         exibirBalaoMensagem("Por favor, preencha todos os campos.", "erro");
     }
+
+    
 }
+
+
